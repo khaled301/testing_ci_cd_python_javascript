bf7906eeaa70d1899fbb85e3dbcd5ae91a4c4140
Khaled Hasan <khaledhasan301@gmail.com>
2022-02-15 16:05:10 +0600

    fixed the prime function based on the unittest result

--- prime.py
+++ prime.py
@@ -1,4 +1,5 @@
 import math
+
 def is_prime(n):
     if n < 2:
         return False
@@ -7,9 +8,8 @@ def is_prime(n):
     #     if n % i == 0:
     #         return False
     
-    for i in range(2, int(math.sqrt(n))):
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
 
     return True
-print(is_prime(6))
