cee34f4d4a2c546fbe8ff16320acf4147e07ffed
Khaled Hasan <khaledhasan301@gmail.com>
2022-02-15 15:58:10 +0600

    added shell script to run all tests together --- added unittest library to run the tests instead of shell script

---
+++ prime.py
@@ -0,0 +1,15 @@
+import math
+def is_prime(n):
+    if n < 2:
+        return False
+    
+    # for i in range(2, n):
+    #     if n % i == 0:
+    #         return False
+    
+    for i in range(2, int(math.sqrt(n))):
+        if n % i == 0:
+            return False
+
+    return True
+print(is_prime(6))
